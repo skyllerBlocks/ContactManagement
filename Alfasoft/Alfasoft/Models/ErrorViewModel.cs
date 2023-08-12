d9be8fa9d09fd4afec7e6ccaa3ecc432d7dd90a3
Gustavo <Gustavo@DESKTOP-6RTVSRL>
2023-08-12 14:39:41 -0300

    insert initial project

---
+++ Alfasoft/Alfasoft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alfasoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
